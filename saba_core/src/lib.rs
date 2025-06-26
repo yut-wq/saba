763de09a65a87e7498f9ab4d49f2bde78cc8359f
yut-wq <println856@gmail.com>
2025-06-27 03:49:30 +0900

    url解析用のライブラリを作成

---
+++ saba_core/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+
+extern crate alloc;
+
+mod url;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
