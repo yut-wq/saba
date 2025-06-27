f8e20c33aec61a497e778f4a02410ab4269b5df4
yut-wq <println856@gmail.com>
2025-06-28 05:32:10 +0900

    IPアドレスの取得ロジックの追加

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,5 +1,7 @@
 extern crate alloc;
-use alloc::string::String;
+use alloc::format;
+use alloc::string::{String, ToString};
+use noli::net::lookup_host;
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -11,6 +13,18 @@ impl HttpClient {
     }
 
     pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
-        todo!("Implement HTTP GET request");
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP address: {:#?}",
+                    e
+                )))
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::Network("Failed to find IP address".to_string()));
+        }
     }
 }
