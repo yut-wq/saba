8ef8f08335827d3908999331270afcb35ccc058d
yut-wq <println856@gmail.com>
2025-06-28 06:26:48 +0900

    httpレスポンスの生成処理を追加

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -1,5 +1,6 @@
 use crate::error::Error;
-use alloc::string::String;
+use alloc::format;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 
 #[derive(Debug, Clone)]
@@ -12,14 +13,76 @@ pub struct HttpResponse {
 }
 
 impl HttpResponse {
-    pub fn new(
-        version: String,
-        status_code: u16,
-        reason: String,
-        headers: Vec<Header>,
-        body: String,
-    ) -> Result<Self, Error> {
-        todo!()
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+        let preprocessed_response = raw_response.trim_start().replace("\r\n", "\n");
+
+        // ステータスラインの取得
+        let (status_line, remaining) = match preprocessed_response.split_once('\n') {
+            Some((s, r)) => (s, r),
+            None => {
+                return Err(Error::Network(format!(
+                    "invalid http response: {}",
+                    preprocessed_response
+                )));
+            }
+        };
+
+        // ヘッダーとボディの分割
+        let (headers, body) = match remaining.split_once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split('\n') {
+                    let splitted_header: Vec<&str> = header.splitn(2, ':').collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim()),
+                    ));
+                }
+                (headers, b)
+            }
+            None => (Vec::new(), remaining),
+        };
+
+        // ステータスラインの解析
+        let statuses: Vec<&str> = status_line.split(' ').collect();
+
+        Ok(Self {
+            version: statuses[0].to_string(),
+            status_code: statuses[1].parse().unwrap_or(404),
+            reason: statuses[2].to_string(),
+            headers,
+            body: body.to_string(),
+        })
+    }
+
+    pub fn version(&self) -> String {
+        self.version.clone()
+    }
+
+    pub fn status_code(&self) -> u16 {
+        self.status_code
+    }
+
+    pub fn reason(&self) -> String {
+        self.reason.clone()
+    }
+
+    pub fn headers(&self) -> &Vec<Header> {
+        &self.headers
+    }
+
+    pub fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    /// ヘッダーの値を取得する
+    pub fn header_value(&self, name: &str) -> Result<String, String> {
+        for header in &self.headers {
+            if header.name == name {
+                return Ok(header.value.clone());
+            }
+        }
+        Err(format!("failed to find {} in headers", name))
     }
 }
 
