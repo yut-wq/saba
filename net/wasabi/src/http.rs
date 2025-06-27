4155aaefd632addfe0bee997396dabfd0417e58f
yut-wq <println856@gmail.com>
2025-06-28 05:53:18 +0900

    HTTPリクエストの送信、レスポンスの受信処理を追加

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,6 +1,7 @@
 extern crate alloc;
 use alloc::format;
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 use noli::net::{lookup_host, TcpStream};
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
@@ -51,5 +52,38 @@ impl HttpClient {
         request.push_str("Accept: text/html\n");
         request.push_str("Connection: close\n");
         request.push('\n');
+
+        // リクエストの送信
+        let _bytes_written = match stream.write(request.as_bytes()) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to send a request to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        // レスポンスの受信
+        let mut received = Vec::new();
+        loop {
+            let mut buffer = [0; 4096];
+            let bytes_read = match stream.read(&mut buffer) {
+                Ok(bytes) => bytes,
+                Err(_) => {
+                    return Err(Error::Network(
+                        "Failed to receive a request from TCP stream".to_string(),
+                    ))
+                }
+            };
+            if bytes_read == 0 {
+                break; // 接続が閉じられた場合はループを抜ける
+            }
+            received.extend_from_slice(&buffer[..bytes_read]);
+        }
+
+        match core::str::from_utf8(&received) {
+            Ok(response) => HttpResponse::new(response.to_string()),
+            Err(e) => Err(Error::Network(format!("Invalid received response: {}", e))),
+        }
     }
 }
