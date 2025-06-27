9e30554b989c40b0c5dd25a8d8dcfca5ff39cf31
yut-wq <println856@gmail.com>
2025-06-28 05:41:12 +0900

    HTTPリクエストの作成

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,7 +1,7 @@
 extern crate alloc;
 use alloc::format;
 use alloc::string::{String, ToString};
-use noli::net::lookup_host;
+use noli::net::{lookup_host, TcpStream};
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -13,6 +13,7 @@ impl HttpClient {
     }
 
     pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        // IPアドレスの解決
         let ips = match lookup_host(&host) {
             Ok(ips) => ips,
             Err(e) => {
@@ -26,5 +27,29 @@ impl HttpClient {
         if ips.len() < 1 {
             return Err(Error::Network("Failed to find IP address".to_string()));
         }
+
+        // TCPストリームの接続
+        let socket_addr = (ips[0], port).into();
+        let mut stream = match TcpStream::connect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to connect to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        // HTTPリクエストの作成
+        let mut request = String::from("GET /");
+        request.push_str(&path);
+        request.push_str(" HTTP/1.1\n");
+
+        // ヘッダの追加
+        request.push_str("Host: ");
+        request.push_str(&host);
+        request.push_str("\n");
+        request.push_str("Accept: text/html\n");
+        request.push_str("Connection: close\n");
+        request.push('\n');
     }
 }
