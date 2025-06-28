a77b2779e5812ba3d21c81082f13e29c8d751d7e
yut-wq <println856@gmail.com>
2025-06-28 17:59:46 +0900

    トークンに関するモジュール追加

--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -4,6 +4,7 @@ extern crate alloc;
 
 pub mod error;
 pub mod http;
+pub mod renderer;
 pub mod url;
 
 pub fn add(left: usize, right: usize) -> usize {
---
+++ saba_core/src/renderer/html/token.rs
@@ -0,0 +1,41 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+use crate::renderer::html::attribute::Attribute;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input: html.chars().collect(),
+            buf: String::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    EndTag {
+        tag: String,
+    },
+    Char(char),
+    EOF,
+}
