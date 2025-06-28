b91a7ffc74bb8b06c157b288eda024120336b1d2
yut-wq <println856@gmail.com>
2025-06-28 17:41:19 +0900

    テスト用のhtmlへアクセスを行うように変更

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use noli::prelude::*;
 
 fn main() {
     let client = HttpClient::new();
-    match client.get("example.com".to_string(), 80, "/".to_string()) {
+    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
         Ok(response) => {
             print!("response:\n{:#?}", response);
         }
