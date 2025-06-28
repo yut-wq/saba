7b4fd6123b03b7a20d6c60ecd4bdcfe6572a4c87
yut-wq <println856@gmail.com>
2025-06-28 17:31:13 +0900

    example.comへのHTTPリクエストを送信するロジックを作成

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 #![no_std]
 #![cfg_attr(not(target_os = "linux"), no_main)]
 
+extern crate alloc;
+
+use crate::alloc::string::ToString;
+use net_wasabi::http::HttpClient;
 use noli::prelude::*;
 
 fn main() {
-    Api::write_string("Hello, world!\n");
-    println!("Hello, from println!");
-    Api::exit(42)
+    let client = HttpClient::new();
+    match client.get("example.com".to_string(), 80, "/".to_string()) {
+        Ok(response) => {
+            print!("response:\n{:#?}", response);
+        }
+        Err(e) => {
+            println!("Error: {:#?}", e);
+        }
+    }
 }
 
 entry_point!(main);
