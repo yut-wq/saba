a0fcb85d220a919bc56458cbddc46f76f2020d77
yut-wq <println856@gmail.com>
2025-06-29 20:11:15 +0900

    `Iterator`の実装の追加

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -14,6 +14,60 @@ pub struct HtmlTokenizer {
 }
 
 impl HtmlTokenizer {
+    fn is_eof(&self) -> bool {
+        self.pos > self.input.len()
+    }
+
+    fn consume_next_input(&self) -> char {
+        todo!()
+    }
+}
+
+impl Iterator for HtmlTokenizer {
+    type Item = HtmlToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        loop {
+            let c = self.consume_next_input();
+
+            match self.state {
+                State::Data => {
+                    if c == '<' {
+                        self.state = State::TagOpen;
+                        continue;
+                    }
+                    if self.is_eof() {
+                        return Some(HtmlToken::EOF);
+                    }
+                    return Some(HtmlToken::Char(c));
+                }
+                State::TagOpen => todo!(),
+                State::EndTagOpen => todo!(),
+                State::TagName => todo!(),
+                State::BeforeAttributeName => todo!(),
+                State::AttributeName => todo!(),
+                State::AfterAttributeName => todo!(),
+                State::BeforeAttributeValue => todo!(),
+                State::AttributeValueDoubleQuoted => todo!(),
+                State::AttributeValueSingleQuoted => todo!(),
+                State::AttributeValueUnquoted => todo!(),
+                State::AfterAttributeValueQuoted => todo!(),
+                State::SelfClosingStartTag => todo!(),
+                State::ScriptData => todo!(),
+                State::ScriptDataLessThanSign => todo!(),
+                State::ScriptDataEndTagOpen => todo!(),
+                State::ScriptDataEndTagName => todo!(),
+                State::TemporaryBuffer => todo!(),
+            }
+        }
+    }
+}
+
+impl HtmlTokenizer {
     pub fn new(html: String) -> Self {
         Self {
             state: State::Data,
