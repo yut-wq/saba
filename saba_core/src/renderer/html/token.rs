38ec1f5e6d54cc79c721c638235ce5085a967f88
yut-wq <println856@gmail.com>
2025-07-01 05:33:49 +0900

    `BeforeAttributeValue`の実装追加

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -240,7 +240,24 @@ impl Iterator for HtmlTokenizer {
                     self.state = State::AfterAttributeName;
                     self.start_new_attribute();
                 }
-                State::BeforeAttributeValue => todo!(),
+                State::BeforeAttributeValue => {
+                    if c == ' ' {
+                        continue;
+                    }
+
+                    if c == '"' {
+                        self.state = State::AttributeValueDoubleQuoted;
+                        continue;
+                    }
+
+                    if c == '\'' {
+                        self.state = State::AttributeValueSingleQuoted;
+                        continue;
+                    }
+
+                    self.reconsume = true;
+                    self.state = State::AttributeValueUnquoted;
+                }
                 State::AttributeValueDoubleQuoted => todo!(),
                 State::AttributeValueSingleQuoted => todo!(),
                 State::AttributeValueUnquoted => todo!(),
