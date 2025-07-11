84832475036bb8ed684c7068d81c998d86961a48
yut-wq <println856@gmail.com>
2025-07-11 23:00:27 +0900

    Node構造体のElement型をStringからElementに変更し、WindowおよびElement構造体を追加

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -1,20 +1,19 @@
 use alloc::rc::Rc;
 use alloc::rc::Weak;
 use alloc::string::String;
+use alloc::vec::Vec;
 use core::cell::RefCell;
+
+use crate::renderer::html::attribute::Attribute;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum NodeKind {
     Document,
-    Element(String),
+    Element(Element),
     Text(String),
 }
 
-#[derive(Debug)]
-pub struct Window {
-    // Windowの実装は今後追加
-}
-
+#[derive(Debug, Clone)]
 pub struct Node {
     pub kind: NodeKind,
     window: Weak<RefCell<Window>>,
@@ -38,7 +37,28 @@ impl Node {
         }
     }
 
-    // Getters
+    pub fn kind(&self) -> NodeKind {
+        self.kind.clone()
+    }
+
+    pub fn get_element(&self) -> Option<Element> {
+        match self.kind {
+            NodeKind::Document | NodeKind::Text(_) => None,
+            NodeKind::Element(ref e) => Some(e.clone()),
+        }
+    }
+
+    pub fn element_kind(&self) -> Option<ElementKind> {
+        match self.kind {
+            NodeKind::Document | NodeKind::Text(_) => None,
+            NodeKind::Element(ref e) => Some(e.kind()),
+        }
+    }
+
+    pub fn window(&self) -> &Weak<RefCell<Window>> {
+        &self.window
+    }
+
     pub fn parent(&self) -> &Weak<RefCell<Node>> {
         &self.parent
     }
@@ -60,6 +80,10 @@ impl Node {
     }
 
     // Setters
+    pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+        self.window = window;
+    }
+
     pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
         self.parent = parent;
     }
@@ -79,4 +103,57 @@ impl Node {
     pub fn set_next_sibling(&mut self, next_sibling: Option<Rc<RefCell<Node>>>) {
         self.next_sibling = next_sibling;
     }
+}
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Element {
+    kind: ElementKind,
+    attributes: Vec<Attribute>,
+}
+
+impl Element {
+    pub fn new(element_name: &str, attributes: Vec<Attribute>) -> Self {
+        Self {
+            kind: ElementKind::from_str(element_name)
+                .expect("failed to convert string to ElementKind."),
+            attributes,
+        }
+    }
+
+    pub fn kind(&self) -> ElementKind {
+        self.kind
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ElementKind {
+    Html,
+    Head,
+    Style,
+    Script,
+    Body,
 }
